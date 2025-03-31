70b4f08c7191a8e992cd65d087ab0046f4289b25
Akanksha Goswami <107531046+goswami6@users.noreply.github.com>
2025-03-31 20:30:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
